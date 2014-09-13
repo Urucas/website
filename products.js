d7b8ca2742bc28e3ad020e3e56d63a5ea79f8b9b
Bruno Alassia <vruno.alassia@gmail.com>
2014-09-13 18:42:06 -0300

    add products descriptions

---
+++ products.js
@@ -0,0 +1,10 @@
+var products = {
+	'cineros' : {
+		title : 'Cineros',
+		desc  : 'La Cartelera de Cine Rosarina en la palma de tu mano!'
+	},
+	'kuesty': { 
+		title : 'Kuesty, Sabes a donde ir!',
+		desc : 'Actualmente sólo para la ciudad de Rosario. Kuesty es una aplicación gratuita para teléfonos móviles que trata de resolver el problema de decidir donde salir a comer o tomar algo. Te ayudará en la búsqueda del restaurant más cercano y a su vez el que más se amolde a tus necesidades. También podrás calificar vos mismo los locales y ver las calificaciones de los demás. Kuesty te dará información de los lugares, distancias a ellos, podrás llamar y reservar directamente desde tu móvil. También podrás hacer amigos nuevos que compartan tus mismos gustos y hacerte fan de los restaurants para recibir promos y descuentos! Todo esto siempre desde la comodidad de tu celular, siempre con vos y al alcance de tu mano!'
+	}
+}
