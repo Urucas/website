6182e4cb1954b975e340e1413f7960e0a0610171
Bruno Alassia <vruno.alassia@gmail.com>
2014-09-13 19:03:44 -0300

    add products description

--- products.js
+++ products.js
@@ -5,6 +5,34 @@ var products = {
 	},
 	'kuesty': { 
 		title : 'Kuesty, Sabes a donde ir!',
-		desc : 'Actualmente sólo para la ciudad de Rosario. Kuesty es una aplicación gratuita para teléfonos móviles que trata de resolver el problema de decidir donde salir a comer o tomar algo. Te ayudará en la búsqueda del restaurant más cercano y a su vez el que más se amolde a tus necesidades. También podrás calificar vos mismo los locales y ver las calificaciones de los demás. Kuesty te dará información de los lugares, distancias a ellos, podrás llamar y reservar directamente desde tu móvil. También podrás hacer amigos nuevos que compartan tus mismos gustos y hacerte fan de los restaurants para recibir promos y descuentos! Todo esto siempre desde la comodidad de tu celular, siempre con vos y al alcance de tu mano!'
+		desc : 'Actualmente s&oacute;lo para la ciudad de Rosario. <br /><br /> Kuesty es una aplicaci&oacute;n gratuita para tel&eacute;fonos m&oacute;viles que trata de resolver el problema de decidir donde salir a comer o tomar algo. Te ayudar&aacute; en la b&uacute;squeda del restaurant m&aacute;s cercano y a su vez el que m&aacute;s se amolde a tus necesidades. <br /><br /> Tambi&eacute;n podr&aacute;s calificar vos mismo los locales y ver las calificaciones de los dem&aacute;s. Kuesty te dar&aacute; informaci&oacute;n de los lugares, distancias a ellos, podr&aacute;s llamar y reservar directamente desde tu m&oacute;vil. Tambi&eacute;n podr&aacute;s hacer amigos nuevos que compartan tus mismos gustos y hacerte fan de los restaurants para recibir promos y descuentos! Todo esto siempre desde la comodidad de tu celular, siempre con vos y al alcance de tu mano!'
+	},
+	'cuandollega' : {
+		title: 'Cuando Llega',
+		desc: 'Cuando llega, la aplicación para todos los Rosarinos que quieran consultar cuando el colectivo de linea pasa por su parada, ahora totalmente renovada, incluye recorridos, noticias y puntos de recarga de la tarjeta sin contacto. <br /><br /> Totalmente renovada. Agregamos puntos de recarga, recorridos y noticias. Arreglados los favoritos. <br /><br /> No utiliza servicio de SMS.'
+	},
+	'ba-verde' : {
+		title : 'BA Ciudad Verde', 
+		desc: 'No tires la basura por la calle! Reciclala, tirala en los contenedores. Busca la Campana Verde o contenedor mas cercano a tu posicion con la aplicacion para Android! Ayuda al medioambiente <br /><br /> Buenos Aires Ciudad Verde, aplicacion movil para Android para buscar el contenedor o campana verde mas cercano, realizada para el #BaHAckatonVerde del GCBA.'
+	},
+	'faltaenvido' : {
+		title : 'Faltaenvido',
+		desc: 'El juego de truco es un tradicional juego de cartas Argentino. Este clasico juego se suele jugar en cualquier momento y lugar donde haya un maso de cartas espa&ntilde;olas. <br /><br /> Alguna vez has estado con cartas y sin papel y lapiz para anotar ? Bueno, FaltaEnvido te hace la vida mas facil dando te la posibilidad de ir anotando el puntaje en tu movil con una interfaz amigable y facil de usar. Asi que ahora bajalo y a usar el anotador de truco.'
+	},
+	'wifime' : {
+		title : 'Wifime', 
+		desc : 'How many times have you gone to a restaurant, hostal or service station where they have Wifi access, and have to wait for someone to attend you to give you the  password. Probably several times, which is why we created a collaborative network for these password to be shared between users. <br /><br />Wifime is simple; walk into a room, open the application and search if any Wifi located where you are exists in our list, if someone has already uploaded the password, press the connect button and you are ready to surf the internet with the local Wifi. You dont have to wait to ask the waiter or to get up to the counter to ask for the password. In case that it is not in our list, once you know it, you add it and when someone else comes to this place, he doesnt have to ask the waiter or come up to the counter. <br /><br /> Its simple, easy, is collaborative. <br /><br /> Wifime is not an application of Wifi hacking passwords, it is a simple collaborative network of users sharing passwords attending places'
+	},
+	'tennis-pal' : {
+		title : 'My Jr. Tennis Pal', 
+		desc : 'My Jr. Tennis Tournament Pal is a MUST HAVE mobile/tablet application to help parents and juniors navigate the competitive world of junior tennis tournaments. Never start another match unprepared again. If you are new to the tournament scene or just want the edge needed in this competitive arena, this application is for you. This app is produced in a video/text format with most of the videos being short and to the point. Anyone can use these drills to help a player warm-up and train. Includes over 50 videos and 100 plus exercises.'
+	},
+	'cleeck' : {
+		title : 'Cleeck',
+		desc : 'Desarrollo de la Plataforma + aplicacion android para Cleeck. <br /><br />Cleeck es una plataforma que permite compartir fotos y comentarios con los invitados de t&uacute; evento y luego disponer del alb&uacute;m para descargar directo a tu pc. <br /><br />De esta manera nadie quedar&aacute; fuera de tu evento, y podr&aacute;s tener esas fotos y comentarios que siempre se guarda cada uno de los asistentes al evento! <br /><br /> Adem&aacute;s, en el evento, podr&aacute;s compartir en pantalla las fotos y los comentarios de tus invitados, es realmente divertido!'
+	},
+	'kriket' : {
+		title : 'Kriket',
+		desc: 'kriket es la herramienta que llega para solucionar el problema de registro e inscripcion en distinto tipo de eventos; desde conferencias, casamientos a exposiciones, kriket te ayudará en la inscripcion y recepcion de los inscriptos a tu evento.<br /><br /> Es simple, creas tu evento (casamiento, conferencia,...), invitás a inscribirse a través de kriket o importás de otra plataforma de eventos (meetup, eventioz, excel, etc) y los recibís en la entrada de tu evento con kriket en una tablet. <br /><br /> Recibilos con una lista digital o generá un “autoregistro” en tu evento, donde cada invitado completa pocos datos, se les toma una foto (opcional) y automáticamente se les imprime una etiqueta con su nombre. <br /><br />Has preparado una conferencia en la que tienes que dar certificados ? Kriket también aborda este tipo de eventos y te da la posibilidad de subir un modelo del certificado al crear el evento, y luego al terminar el mismo, puede imprimir los certificados a los presentes. <br /><br />Sin listas en papel, mucho mas elegante.'
 	}
 }
