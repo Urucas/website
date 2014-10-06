1cf95979693af27dc8de282c8932e618c3d17595
Bruno Alassia <vruno.alassia@gmail.com>
2014-10-05 22:07:46 -0300

    add product tags

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 var products = {
 	'cineros' : {
 		title : 'Cineros',
-		desc  : 'La Cartelera de Cine Rosarina en la palma de tu mano!<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.cineros" target="_blank">Cineros en Google Play</a><br /><a href="https://itunes.apple.com/us/app/cineros/id838900914?l=es&mt=8" target="_blank">Cineros en AppStore</a>'
+		desc  : 'La Cartelera de Cine Rosarina en la palma de tu mano!<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.cineros" target="_blank">Cineros en Google Play</a><br /><a href="https://itunes.apple.com/us/app/cineros/id838900914?l=es&mt=8" target="_blank">Cineros en AppStore</a><br /><br /><span>html</span><span>javascript</span><span>phonegap</span><span>android</span><span>ios</span>'
 	},
 	'kuesty': { 
 		title : 'Kuesty, Sabes a donde ir!',
@@ -9,27 +9,27 @@ var products = {
 	},
 	'cuandollega' : {
 		title: 'Cuando Llega',
-		desc: 'Cuando llega, la aplicación para todos los Rosarinos que quieran consultar cuando el colectivo de linea pasa por su parada, ahora totalmente renovada, incluye recorridos, noticias y puntos de recarga de la tarjeta sin contacto. <br /><br /> Totalmente renovada. Agregamos puntos de recarga, recorridos y noticias. Arreglados los favoritos. <br /><br /> No utiliza servicio de SMS.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.cuandollegaandroid" target="_blank">Cuando llega en Google Play</a><br /><a href="https://itunes.apple.com/us/app/cuando-llega/id509324541?l=es&mt=8" target="_blank">Cuando llega en App Store</a>'
+		desc: 'Cuando llega, la aplicación para todos los Rosarinos que quieran consultar cuando el colectivo de linea pasa por su parada, ahora totalmente renovada, incluye recorridos, noticias y puntos de recarga de la tarjeta sin contacto. <br /><br /> Totalmente renovada. Agregamos puntos de recarga, recorridos y noticias. Arreglados los favoritos. <br /><br /> No utiliza servicio de SMS.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.cuandollegaandroid" target="_blank">Cuando llega en Google Play</a><br /><a href="https://itunes.apple.com/us/app/cuando-llega/id509324541?l=es&mt=8" target="_blank">Cuando llega en App Store</a><br /><br /><span>html</span><span>javascript</span><span>phonegap</span><span>gelocation</span><span>android</span><span>ios</span><span>symbian</span><span>series40</span>'
 	},
 	'ba-verde' : {
 		title : 'BA Ciudad Verde', 
-		desc: 'No tires la basura por la calle! Reciclala, tirala en los contenedores. Busca la Campana Verde o contenedor mas cercano a tu posicion con la aplicacion para Android! Ayuda al medioambiente <br /><br /> Buenos Aires Ciudad Verde, aplicacion movil para Android para buscar el contenedor o campana verde mas cercano, realizada para el #BaHAckatonVerde del GCBA.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.bamaslimpia" target="_blank">BA Ciudad Verde en Google Play</a>'
+		desc: 'No tires la basura por la calle! Reciclala, tirala en los contenedores. Busca la Campana Verde o contenedor mas cercano a tu posicion con la aplicacion para Android! Ayuda al medioambiente <br /><br /> Buenos Aires Ciudad Verde, aplicacion movil para Android para buscar el contenedor o campana verde mas cercano, realizada para el #BaHAckatonVerde del GCBA.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.bamaslimpia" target="_blank">BA Ciudad Verde en Google Play</a><br /><br /><span>java</span><span>android</span><span>gelocation</span>'
 	},
 	'faltaenvido' : {
 		title : 'Faltaenvido',
-		desc: 'El juego de truco es un tradicional juego de cartas Argentino. Este clasico juego se suele jugar en cualquier momento y lugar donde haya un maso de cartas espa&ntilde;olas. <br /><br /> Alguna vez has estado con cartas y sin papel y lapiz para anotar ? Bueno, FaltaEnvido te hace la vida mas facil dando te la posibilidad de ir anotando el puntaje en tu movil con una interfaz amigable y facil de usar. Asi que ahora bajalo y a usar el anotador de truco.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.faltaenvido" target="_blank">Faltaenvido en Google Play</a>'
+		desc: 'El juego de truco es un tradicional juego de cartas Argentino. Este clasico juego se suele jugar en cualquier momento y lugar donde haya un maso de cartas espa&ntilde;olas. <br /><br /> Alguna vez has estado con cartas y sin papel y lapiz para anotar ? Bueno, FaltaEnvido te hace la vida mas facil dando te la posibilidad de ir anotando el puntaje en tu movil con una interfaz amigable y facil de usar. Asi que ahora bajalo y a usar el anotador de truco.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.faltaenvido" target="_blank">Faltaenvido en Google Play</a><br /><br /><span>html</span><span>javascript</span><span>phonegap</span><span>android</span><span>ios</span><span>symbian</span>'
 	},
 	'wifime' : {
 		title : 'Wifime', 
-		desc : 'How many times have you gone to a restaurant, hostal or service station where they have Wifi access, and have to wait for someone to attend you to give you the  password. Probably several times, which is why we created a collaborative network for these password to be shared between users. <br /><br />Wifime is simple; walk into a room, open the application and search if any Wifi located where you are exists in our list, if someone has already uploaded the password, press the connect button and you are ready to surf the internet with the local Wifi. You dont have to wait to ask the waiter or to get up to the counter to ask for the password. In case that it is not in our list, once you know it, you add it and when someone else comes to this place, he doesnt have to ask the waiter or come up to the counter. <br /><br /> Its simple, easy, is collaborative. <br /><br /> Wifime is not an application of Wifi hacking passwords, it is a simple collaborative network of users sharing passwords attending places<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.wifime" target="_blank">Wifime en Google Play</a>'
+		desc : 'How many times have you gone to a restaurant, hostal or service station where they have Wifi access, and have to wait for someone to attend you to give you the  password. Probably several times, which is why we created a collaborative network for these password to be shared between users. <br /><br />Wifime is simple; walk into a room, open the application and search if any Wifi located where you are exists in our list, if someone has already uploaded the password, press the connect button and you are ready to surf the internet with the local Wifi. You dont have to wait to ask the waiter or to get up to the counter to ask for the password. In case that it is not in our list, once you know it, you add it and when someone else comes to this place, he doesnt have to ask the waiter or come up to the counter. <br /><br /> Its simple, easy, is collaborative. <br /><br /> Wifime is not an application of Wifi hacking passwords, it is a simple collaborative network of users sharing passwords attending places<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.wifime" target="_blank">Wifime en Google Play</a><br /><br /><span>java</span><span>android</span>'
 	},
 	'tennis-pal' : {
 		title : 'My Jr. Tennis Pal', 
-		desc : 'My Jr. Tennis Tournament Pal is a MUST HAVE mobile/tablet application to help parents and juniors navigate the competitive world of junior tennis tournaments. Never start another match unprepared again. If you are new to the tournament scene or just want the edge needed in this competitive arena, this application is for you. This app is produced in a video/text format with most of the videos being short and to the point. Anyone can use these drills to help a player warm-up and train. Includes over 50 videos and 100 plus exercises.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.myjrtennispal" target="_blank">My Jr. Tennis Pal en Google Play</a>'
+		desc : 'My Jr. Tennis Tournament Pal is a MUST HAVE mobile/tablet application to help parents and juniors navigate the competitive world of junior tennis tournaments. Never start another match unprepared again. If you are new to the tournament scene or just want the edge needed in this competitive arena, this application is for you. This app is produced in a video/text format with most of the videos being short and to the point. Anyone can use these drills to help a player warm-up and train. Includes over 50 videos and 100 plus exercises.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.myjrtennispal" target="_blank">My Jr. Tennis Pal en Google Play</a><br /><br /><span>html</span><span>javascript</span><span>phonegap</span><span>android</span><span>ios</span>'
 	},
 	'cleeck' : {
 		title : 'Cleeck',
-		desc : 'Desarrollo de la Plataforma + aplicacion android para Cleeck. <br /><br />Cleeck es una plataforma que permite compartir fotos y comentarios con los invitados de t&uacute; evento y luego disponer del alb&uacute;m para descargar directo a tu pc. <br /><br />De esta manera nadie quedar&aacute; fuera de tu evento, y podr&aacute;s tener esas fotos y comentarios que siempre se guarda cada uno de los asistentes al evento! <br /><br /> Adem&aacute;s, en el evento, podr&aacute;s compartir en pantalla las fotos y los comentarios de tus invitados, es realmente divertido!<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.cleeck" target="_blank">Cleeck en Google Play</a>'
+		desc : 'Desarrollo de la Plataforma + aplicacion android para Cleeck. <br /><br />Cleeck es una plataforma que permite compartir fotos y comentarios con los invitados de t&uacute; evento y luego disponer del alb&uacute;m para descargar directo a tu pc. <br /><br />De esta manera nadie quedar&aacute; fuera de tu evento, y podr&aacute;s tener esas fotos y comentarios que siempre se guarda cada uno de los asistentes al evento! <br /><br /> Adem&aacute;s, en el evento, podr&aacute;s compartir en pantalla las fotos y los comentarios de tus invitados, es realmente divertido!<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.cleeck" target="_blank">Cleeck en Google Play</a><br /><br /><span>java</span><span>android</span><span>html</span><span>javascript</span><span>php</span><span>mysql</span><span>zend frameword</span>'
 	},
 	'kriket' : {
 		title : 'Kriket',
@@ -37,19 +37,19 @@ var products = {
 	},
 	'eztenapp': {
 		title : 'eztenapp',
-		desc: 'Eztenapp Beta for Android, an easy way to manage your slides presentations from your android. <br />Supports: <br />*Slides.com(reveal.js),<br />*Slideshare.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.library.sting" target="_blank">eztenapp en Google Play</a>'
+		desc: 'Eztenapp Beta for Android, an easy way to manage your slides presentations from your android. <br />Supports: <br />*Slides.com(reveal.js),<br />*Slideshare.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.urucas.library.sting" target="_blank">eztenapp en Google Play</a><br /><br /><span>java</span><span>android</span><span>node</span><span>socket.io</span>'
 	},
 	'programa-you': {
 		title: 'Programa YOU',
-		desc: 'Sitio web para Programayou, el programa dirigido a YOU está dirigido a jóvenes profesionales (mayores de 24 años), recibidos o con más de 3 años de experiencia en Áreas vinculadas a la Comunicación, Publicidad, Marketing, Comercialización, Administración o afines (no excluyente).<br /><br /><a href="//programayou.com.ar" target="_blank">programayou.com.ar</a>'
+		desc: 'Sitio web para Programayou, el programa dirigido a YOU está dirigido a jóvenes profesionales (mayores de 24 años), recibidos o con más de 3 años de experiencia en Áreas vinculadas a la Comunicación, Publicidad, Marketing, Comercialización, Administración o afines (no excluyente).<br /><br /><a href="//programayou.com.ar" target="_blank">programayou.com.ar</a><br /><br /><span>html</span><span>javascript</span><span>php</span><span>mysql</span><span>zend frameword</span>'
 	},
 	'yedro': {
 		title: 'Yedro',
-		desc: 'Sitio web para Yedro Comunicacion S.R.L, la empresa de capitales argentinos con 30 años de trayectoria en el mercado, especializada es la fabricación de antenas y venta de equipos de radios de dos vías.<br /><br /><a href="//yedro.com.ar" target="_blank">yedro.com.ar</a>'
+		desc: 'Sitio web para Yedro Comunicacion S.R.L, la empresa de capitales argentinos con 30 años de trayectoria en el mercado, especializada es la fabricación de antenas y venta de equipos de radios de dos vías.<br /><br /><a href="//yedro.com.ar" target="_blank">yedro.com.ar</a><br /><br /><span>html</span><span>javascript</span><span>php</span><span>mysql</span><span>zend frameword</span>'
 	},
 	'checkboxx': {
 		title: 'Checkboxx', 
-		desc: 'Project built in Music Hack Day San Francisco 2014. Allows users to login though rdio, and based on their music history as a whole group we will play the most common genre for the group of people to enjoy.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.music.mybarr" target="_blank">Checkboxx en Google Play</a>'
+		desc: 'Project built in Music Hack Day San Francisco 2014. Allows users to login though rdio, and based on their music history as a whole group we will play the most common genre for the group of people to enjoy.<br /><br /><a href="https://play.google.com/store/apps/details?id=com.music.mybarr" target="_blank">Checkboxx en Google Play</a><br /><br /><span>java</span><span>android</span>'
 	}, 
 	'pasearte': {
 		title: 'Pasearte',
@@ -57,6 +57,6 @@ var products = {
 	},
 	'm8v': {
 		title: 'Milochoveinte',
-		desc: '<a href="http://milochoveinte.com.ar/" target="_blank">milochoveinte.com.ar</a>'
+		desc: '<a href="http://milochoveinte.com.ar/" target="_blank">milochoveinte.com.ar</a><br /><br /><span>html</span><span>javascript</span><span>php</span><span>mysql</span><span>zend frameword</span><span>html</span>'
 	}
 }
