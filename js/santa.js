6af244a4f00956965bcbb0332cd318a63809e9aa
Bruno Alassia <vruno.alassia@gmail.com>
2014-12-24 10:49:28 -0300

    add santa

---
+++ js/santa.js
@@ -0,0 +1,95 @@
+/**
+ * Urucas animatedSprite.js
+ * https://github.com/Urucas/animatedSprite.js
+ */
+var _as = _as || new _asController();
+
+function _asController() {
+
+	this.sprites = [];
+
+	this.addSprite = function(id, params) {
+		this.sprites.push(new _asInstance(id, params));
+	}
+
+	this.interval = 100;
+	this.intervalID = setInterval(function(){
+		for(var i=0; i < _as.sprites.length; i++) {
+			_as.sprites[i].animate();
+		}
+	}, this.interval);
+}
+
+function _asInstance(id, params){
+	this.el = document.getElementById(id);
+
+	this.w = params.width;
+	this.h = params.height;
+	this.cols   = params.cols;
+	this.rows   = params.rows;
+	this.image  = params.image;
+	this.interval   = params.interval || 100;
+	console.log(this.interval);
+
+	this.posX = 0;
+	this.posY = 0;
+
+	this.moveLeft = this.cols != 0 ? this.w/this.cols : 0;
+	this.moveDown = this.rows != 0 ? this.h/this.rows : 0;
+
+	this.el.style.width=(this.w/this.cols)+"px";
+	this.el.style.height=(this.h/this.rows)+"px";
+	this.el.style.backgroundImage = "url('"+this.image+"')";
+	this.el.style.backgroundRepeat = "no-repeat";
+	this.el.style.backgroundPosition = "0% 0%";
+
+	this.animate = function() {
+		this.posX = this.posX-this.moveLeft;
+		if(this.posX <= -this.w) {
+			this.posX = 0;
+			this.posY = this.posY-this.moveDown;
+			if(this.posY <= -this.h) {
+				this.posY = 0;
+			}
+
+		}
+		this.el.style.backgroundPosition = this.posX+"px "+this.posY+"px";
+	}
+}
+
+
+/*
+ * Santa.js
+ */
+$(document).ready(function(){
+	$('#santa').ScrollTo();
+
+	_as.addSprite("santa",{
+		width:600,
+		height:600,
+		cols:4,
+		rows:4,
+		image:"images/santa.jpg",
+		interval: 10
+	});
+
+	var w = window.screen.availWidth;
+
+	var t = setInterval(function(){
+		var pos = parseInt(santa.style.left);
+		if(pos > w+150) {
+			window.clearInterval(t);
+		}else{
+			santa.style.left = (pos+10)+"px";
+		}
+	}, 100);
+
+});
+
+santa.style.position = "relative";
+santa.style.left= "-150px";
+santa.style.top = "-32px";
+santa.style.display = "inline-block";
+
+
+
